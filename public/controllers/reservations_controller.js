dbec874c9522d84b5dc724ed6a5362a1336adc1c
Thomas Beldame <thomas@HomeServer>
2017-10-18 07:54:21 +0200

    Added a message to the reservation page when there are no reservations

--- public/controllers/reservations_controller.js
+++ public/controllers/reservations_controller.js
@@ -1,11 +1,23 @@
 var stationfTest = angular.module('stationfTest');
 
 stationfTest.controller('reservationsCtrl', function($scope, $http, $mdToast) {
+	$scope.noReservations = false;
+
 	$http.get('/api/reservations')
 	.then(function success(response) {
-			$scope.reservations = response.data.reservations;
+		$scope.reservations = response.data.reservations;
+		if (response.data.reservations.length === 0)
+			$scope.noReservations = true;
+		else
+			$scope.noReservations = false;
 	}, function error(response) {
-		console.error(response)
+		$mdToast.show(
+				$mdToast.simple()
+				.theme('error-toast')
+				.position('top right')
+				.textContent('Une erreur est survenue')
+				.hideDelay(3000)
+		);
 	});
 
 	$scope.deleteReservation = function (reservation) {
